f7fe9a5388a9ddfcc602e0cc3e3c6da084fadb14
akinayoola <akingunloye006@gmail.com>
2023-06-17 04:15:18 -0700

    posorneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,9 @@
 	printf(the number; "%d", n);
 	if (n > 0) 
 	{printf("is positive\n");}
-	if (n == 0)
+	else if (n == 0)
 	{printf("is zero\n");}
-	if (n < 0)
+	else (n < 0)
 	{printf("is negative\n");}
 	return (0);
 }
