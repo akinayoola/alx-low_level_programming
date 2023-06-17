074bd1775c26781f3bff47a8f1356ff8487019b2
akinayoola <akingunloye006@gmail.com>
2023-06-17 00:59:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-	int main(void) {
+	int main(void)
+	/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+	{
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 	}
