3d302232729386393589eb92ca8e2029e0430344
akinayoola <akingunloye006@gmail.com>
2023-06-17 05:16:43 -0700

    posorneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,12 @@
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf(the number; "%d", n);
-	if (n > 0) 
-	{printf("is positive\n");}
-	else if (n == 0)
-	{printf("is zero\n");}
-	else (n < 0)
-	{printf("is negative\n");}
+	if (n > 0) {
+		printf("is positive\n");
+	} else if (n == 0){
+		printf("is zero\n");
+	} else (n < 0){
+		printf("is negative\n");
+	}
 	return (0);
 }
