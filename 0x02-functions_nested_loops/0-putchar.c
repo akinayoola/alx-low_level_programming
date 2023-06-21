bc259941e51f3277a1c4a2196cfbc1e8fde9fc66
akinayoola <akingunloye006@gmail.com>
2023-06-21 16:15:36 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.c"
+/**
+ * print the program _putchar
+ *
+ * Return: always (0)
+ */
+
+	int _putchar(char c) {
+		return (_putchar(c);
+	}
+{
+	_putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+task 0 is a program that prints _putchar, followed by a new line.
