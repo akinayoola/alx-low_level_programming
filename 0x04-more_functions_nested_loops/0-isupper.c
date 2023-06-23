90d82e5e80648c6b58c9177fb3fef7f9cc23ad00
akinayoola <akingunloye006@gmail.com>
2023-06-23 08:15:27 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - print uppercase character
+ *@c: number to be determined
+ *
+ * Return: 1 if uppercase and 0 if otherwise
+ */
+
+int _isupper(int c);
+{
+	if (c <= 'A' && c >= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for a digit
+ * @c: is the number to be determined
+ *
+ * Return: 1 if c is a digit or 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1); }
+	{ return (0); }
+}
