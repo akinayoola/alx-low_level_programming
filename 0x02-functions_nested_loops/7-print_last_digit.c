13fc14f2ed6c1c233619cbc550f2bd14c267e275
akinayoola <akingunloye006@gmail.com>
2023-06-23 04:26:03 -0700

    lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints las digit of a number
+ * @b: is the number
+ *
+ * Return: 0
+ */
+
+int print_last_digit(int b)
+{
+	if (b % 10 < 0)
+	{
+	_putchar(b); }
+
+	return (0);
+}
