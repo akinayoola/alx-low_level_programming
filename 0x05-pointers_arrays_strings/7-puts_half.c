1200b57da0055074a3b146ea2dbd898f44c8833b
akinayoola <akingunloye006@gmail.com>
2023-06-26 13:11:19 -0700

    halfstring

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half -  prints half of a string
+ * @str: string character
+ *
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start_index;
+
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+
+	while (str[start_index] != '\0')
+	{
+		putchar(str[start_index]);
+		start_index++;
+	}
+	putchar('\n');
+}
