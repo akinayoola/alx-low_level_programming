e8b5f19dbc3acfa0ff123804abe051d6c2f67bf4
akinayoola <akingunloye006@gmail.com>
2023-06-26 12:47:42 -0700

    Fcharacter

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,8 +14,9 @@ void rev_string(char *s)
 	int length = strlen(s);
 	int i, j;
 	char temp;
-for: (i = 0; j = length - 1; i++)
-	j--;
+	for (i = 0; i++)
+		j = length;
+		j--;
 	{ temp = s[i];
 		s[i] = s[j];
 		s[j] = temp; }
