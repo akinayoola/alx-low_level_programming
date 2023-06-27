208670a3a5bdfffb16569ac408486cf9d443ec6b
akinayoola <akingunloye006@gmail.com>
2023-06-27 13:20:03 -0700

    revstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,9 +14,10 @@ void rev_string(char *s)
 	int length = strlen(s);
 	int i, j;
 	char temp;
-	for (i = 0; i++)
-		j = length;
-		j--;
+
+	for (i = 0; i++; j--)
+		j = length - 1;
+
 	{ temp = s[i];
 		s[i] = s[j];
 		s[j] = temp; }
