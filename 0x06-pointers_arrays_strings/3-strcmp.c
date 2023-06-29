bfe1c84d3b889f261be9ccbbcdb775821af30125
akinayoola <akingunloye006@gmail.com>
2023-06-29 06:01:03 -0700

    comparestring

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2); }
+		s1++;
+		s2++;
+	}
+	if (*s1 == '\0' && *s2 != '\0')
+	{return (-1); }
+	else if (*s1 != '\0' && *s2 == '\0')
+	{return (1); }
+	else
+	{return (0); }
+}
+
