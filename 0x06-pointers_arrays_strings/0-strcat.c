a87499b477dfa8dbcf51a3d59feb9899d1885b0a
akinayoola <akingunloye006@gmail.com>
2023-06-29 02:23:14 -0700

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: destination variable
+ * @src: source variable
+ * REturn: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '0')
+	{ ptr++; }
+
+	while (*src != '0')
+	{*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+task 0 is a function that concatenates two strings.
