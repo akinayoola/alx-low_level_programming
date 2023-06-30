f61bc1910a55690f81df8e742a262ee85dbddd17
akinayoola <akingunloye006@gmail.com>
2023-06-30 03:46:23 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print base number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+
+	for (i = 0; i < 6; i++)
+	{
+		putchar('a' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
