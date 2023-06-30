54b4b06b371d530d5cf69104179f3ae610e5a281
akinayoola <akingunloye006@gmail.com>
2023-06-30 03:37:54 -0700

    printcomb

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function 
+ *
+ * Return: 0
+ */
+
+int main ()
+{
+	char i;
+
+	if (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i); }
+	putchar('\n');
+	return (0);
+}
