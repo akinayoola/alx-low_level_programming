05c11ffc05ce8dcc388f61ec4eaddc61ee92483e
akinayoola <akingunloye006@gmail.com>
2023-07-03 11:35:28 -0700

    substring

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strpbrk - function that searches a string for any of a set of bytes
+ * @s: character variable
+ * @accept: variable
+ *
+ * Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != 0)
+	{
+		for (int i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i]
+					{ return (s);
+					}
+					}
+					s++;
+					}
+					return (NULL0;
+						}
