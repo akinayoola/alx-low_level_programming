d68bfe4299de39943a7babcfd249abe7e84b9257
akinayoola <akingunloye006@gmail.com>
2023-07-07 15:26:16 -0700

    myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - print the name
+ *@argc: argument count
+ *@argv: pointer argument
+ *
+ * Return: 0
+ */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((__unused__)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return(0);
