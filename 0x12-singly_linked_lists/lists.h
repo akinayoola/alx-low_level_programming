c3d304a6a9521906d2cf071bfe793d8683f4f391
akinayoola <akingunloye006@gmail.com>
2023-07-27 08:51:00 -0700

    list len

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,13 +15,14 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 
 size_t print_list(const list_t *h);
 int _putchar(char c);
+size_t list_len(const list_t *h);
 
 #endif /* LIST_H */
