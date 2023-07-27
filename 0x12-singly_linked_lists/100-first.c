f92370252c449b7c7dc33c568dbce4497edb4b41
akinayoola <akingunloye006@gmail.com>
2023-07-27 09:32:51 -0700

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * premain - runs before the main to print the output
+ *
+ * Return: 0
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
